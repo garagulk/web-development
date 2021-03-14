6de3503b1214204eb7e00daba02e0f39c3029a8c
garagulk <79750010+garagulk@users.noreply.github.com>
2021-03-14 23:42:13 +0300

    +

---
+++ lw5/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="css/style.css" rel="stylesheet"/>
+</head>
+<body>
+<div class="top-block">
+    <img src="images/jane.png" class="top-block__image" alt="jane">
+    <div class="top-block__quote">
+        <img src="images/quotes.png" class="top-block__quote_image" alt="quotes">
+        <span class="top-block__quote_text">
+        Мы берем на себя много, потому что мало чего боимся
+        </span>
+        <span class="top-block__quote_signature">
+            — Том Демарко. Deadline
+        </span>
+    </div>
+</div>
+<div class="low-block">
+    <div class="low-block__left">
+    <span class="low-block__left_title1">
+        Любимые писатели:
+    </span>
+        <ul class="low-block__left_list">
+            <li>
+                Айзек Азимов
+            </li>
+            <li>
+                Говард Лавкрафт
+            </li>
+            <li>
+                Дмитрий Глуховский
+            </li>
+            <li>
+                Чак Паланик
+            </li>
+        </ul>
+        <span class="low-block__left_title2">
+            Любимые фильмы:
+        </span>
+        <ol class="low-block__left_list2">
+            <li>
+                Шоу Трумэна
+            </li>
+            <li>
+                Малхолланд Драйв
+            </li>
+            <li>
+                Семь жизней
+            </li>
+            <li>
+                Гравитация
+            </li>
+        </ol>
+    </div>
+
+</div>
+</body>
+</html>
