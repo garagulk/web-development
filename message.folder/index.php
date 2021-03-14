fba6b98006da98fa0bf3b84df4776f2e86edd212
garagulk <79750010+garagulk@users.noreply.github.com>
2021-03-14 15:41:15 +0300

    Обновления для lw5

--- message.folder/index.php
+++ message.folder/index.php
@@ -1,6 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<body>
-    <script src="script.js"></script>
-</body>
-</html>
+<?php
+    $PervNumber = 1;
+    $CurrNumber = 1;
+    echo $CurrNumber;
+    echo " ";
+    WHILE ($CurrNumber < 50)
+    {
+        $temp = $CurrNumber;
+        echo $CurrNumber;
+        echo " ";
+        $CurrNumber = $CurrNumber + $PervNumber;
+        $PervNumber = $temp;
+    }
+    
+    echo "/";
+    $Numbers = ['1', '2', '3'];
+    echo $Numbers[0];
+    $Numbers[] = '4';
+    echo "/";
+    echo $Numbers[3];
+    $Numbers[4] = '5';
+    echo "/";
+    echo $Numbers[4];
+    $Numbers[5] = "Тут лежит текст";
+    echo "/";
+    echo $Numbers[5];
+    $Numbers[6] = 1000;
+    echo "/";
+    echo $Numbers[6];
+    
+    echo "/";
+    echo $Numbers[6];
+   
+    
